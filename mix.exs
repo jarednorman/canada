640158aa8f4159af04c41b267d79884f582d1231
Sebastian A. Espindola <sespindola@gmail.com>
2016-01-30 11:53:21 -0300

    loosened elixir requirements

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Canada.Mixfile do
   def project do
     [app: :canada,
      version: "1.0.0",
-     elixir: "~> 1.0",
+     elixir: ">= 1.0",
      package: package,
      consolidate_protocols: Mix.env != :test,
      description: """
