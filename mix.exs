15ec81fa1ec6ca78970e1847f1f8d2d93fcf3392
Jared Norman <jared@creepywizard.com>
2016-08-23 21:32:17 -0700

    v1.0.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Canada.Mixfile do
 
   def project do
     [app: :canada,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.0",
      package: package,
      consolidate_protocols: Mix.env != :test,
