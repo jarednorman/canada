ddb19e9c5ffb4867a581abd313d9547b35d52900
Kian-Meng Ang <kianmeng@cpan.org>
2021-04-21 23:27:18 +0800

    Misc doc changes
    Besides other documentation changes, this commit ensures the generated
    HTML doc for HexDocs.pm will become the main source doc for this Elixir
    library which leverage on latest ExDoc features.

--- mix.exs
+++ mix.exs
@@ -1,35 +1,57 @@
 defmodule Canada.Mixfile do
   use Mix.Project
 
+  @source_url "https://github.com/jarednorman/canada"
+  @version "2.0.0"
+
   def project do
-    [app: :canada,
-     version: "2.0.0",
-     elixir: "~> 1.0",
-     package: package(),
-     consolidate_protocols: Mix.env != :test,
-     description: """
-       A DSL for declarative permissions
-     """,
-     deps: deps(),
-     docs: docs()]
+    [
+      app: :canada,
+      version: @version,
+      elixir: "~> 1.0",
+      consolidate_protocols: Mix.env() != :test,
+      package: package(),
+      deps: deps(),
+      docs: docs()
+    ]
   end
 
   def package do
-    [maintainers: ["Jared Norman"],
-     contributors: ["Jared Norman"],
-     licenses: ["MIT"],
-     links: %{github: "https://github.com/jarednorman/canada"}]
+    [
+      description: "A DSL for declarative permissions",
+      maintainers: ["Jared Norman"],
+      contributors: ["Jared Norman"],
+      licenses: ["MIT"],
+      links: %{
+        GitHub: @source_url
+      }
+    ]
   end
 
   def application do
-    [applications: []]
+    [
+      applications: []
+    ]
   end
 
   defp deps do
-    [{:ex_doc, ">= 0.0.0", only: :dev}]
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
   end
 
   defp docs do
-    [extras: ["README.md"]]
+    [
+      extras: [
+        "CHANGELOG.md",
+        "LICENSE.md": [title: "License"],
+        "README.md": [title: "Readme"]
+      ],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      api_reference: false,
+      formatters: ["html"]
+    ]
   end
 end
